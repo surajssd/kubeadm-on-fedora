33146520a743e7b6b2a4c38a6e3f77b47487a47c
Suraj Deshmukh <surajd.service@gmail.com>
2019-07-11 09:55:03 +0530

    master: Source common bash script
    Signed-off-by: Suraj Deshmukh <surajd.service@gmail.com>

--- master.bash
+++ master.bash
@@ -4,5 +4,10 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+readonly root=$(dirname "${BASH_SOURCE[0]}")
+
+# shellcheck source=/dev/null
+source "${root}/common.bash"
+
 setup_fedora_machine
 kubeadm init --pod-network-cidr=192.168.0.0/16
