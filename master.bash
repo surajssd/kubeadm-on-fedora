c48c21482c276bea16129395f530eeec1661bad1
Suraj Deshmukh <surajd.service@gmail.com>
2019-07-11 11:04:02 +0530

    master: add kubeadm join output
    Signed-off-by: Suraj Deshmukh <surajd.service@gmail.com>

--- master.bash
+++ master.bash
@@ -20,3 +20,7 @@ kubeadm init --pod-network-cidr=192.168.0.0/16
 copy_kubeconfig "/root"
 copy_kubeconfig "/home/vagrant"
 kubectl apply -f https://docs.projectcalico.org/v3.8/manifests/calico.yaml
+echo "Run following command on workers:"
+join_cmd="$(kubeadm token create --print-join-command)"
+echo "vagrant ssh w0 -c \"${join_cmd}\""
+echo "vagrant ssh w1 -c \"${join_cmd}\""
