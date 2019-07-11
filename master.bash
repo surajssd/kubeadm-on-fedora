db707283e3fdd4a3a49c3b0063b1c225d8a7183c
Suraj Deshmukh <surajd.service@gmail.com>
2019-07-11 11:21:18 +0530

    master: set the right kubeconfig permissions
    Signed-off-by: Suraj Deshmukh <surajd.service@gmail.com>

--- master.bash
+++ master.bash
@@ -12,15 +12,19 @@ source "${root}/common.bash"
 function copy_kubeconfig() {
   mkdir -p "${1}"/.kube
   sudo cp -i /etc/kubernetes/admin.conf "${1}"/.kube/config
-  sudo chown $(id -u):$(id -g) "${1}"/.kube/config
+  chmod +rw "${1}"/.kube/config
 }
 
 setup_fedora_machine
 kubeadm init --pod-network-cidr=192.168.0.0/16
 copy_kubeconfig "/root"
 copy_kubeconfig "/home/vagrant"
+
+# setup networking
 kubectl apply -f https://docs.projectcalico.org/v3.8/manifests/calico.yaml
+
+# instructions for workers to join master
 echo "Run following command on workers:"
-join_cmd="$(kubeadm token create --print-join-command)"
+join_cmd="sudo $(kubeadm token create --print-join-command)"
 echo "vagrant ssh w0 -c \"${join_cmd}\""
 echo "vagrant ssh w1 -c \"${join_cmd}\""
